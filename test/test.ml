4549e3a92a3a21cbe9d1f383df86510e75bd7f51
Levi Roth <levimroth@gmail.com>
2018-01-08 14:30:59 -0500

    Add more tests for parser

--- test/test.ml
+++ test/test.ml
@@ -1,8 +1,9 @@
+open Base
+
 let%test_module "parser tests" = (
   module
   struct
     open Deductive_logic
-    open Expression
 
     let equal = [%compare.equal: Expression.t]
 
@@ -11,8 +12,20 @@ let%test_module "parser tests" = (
       |> Lexing.from_string
       |> Parser.line Lexer.read
 
-    let%test _ = equal (parse_string "-(p & q)") (Neg (Conj (Prop 'p', Prop 'q')))
-    let%test _ = equal (parse_string "-p & q") (Conj (Neg (Prop 'p'), (Prop 'q')))
+    let test_cases = [
+      "p", "(Prop p)";
+      "(p)", "(Prop p)";
+      "p & q", "(Conj (Prop p) (Prop q))";
+      "p | q", "(Disj (Prop p) (Prop q))";
+      "p -> q", "(Cond (Prop p) (Prop q))";
+      "-p", "(Neg (Prop p))";
+      "-(p & q)", "(Neg (Conj (Prop p) (Prop q)))";
+      "-p & q", "(Conj (Neg (Prop p)) (Prop q))";
+    ]
+
+    let%test _ = List.for_all test_cases ~f:(fun (input, output) ->
+        equal (parse_string input) (Sexplib.Sexp.of_string output |> Expression.t_of_sexp)
+      )
   end)
 
 let () =

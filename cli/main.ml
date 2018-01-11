8159660f7627530099a37e67186c1f1aad082c0d
Levi Roth <levimroth@gmail.com>
2018-01-11 14:18:07 -0500

    Add stdin option to cli

--- cli/main.ml
+++ cli/main.ml
@@ -2,19 +2,24 @@ open Base
 open Stdio
 open Deductive_logic
 
-let construct_deduction filename =
-  In_channel.with_file filename ~f:(fun ic ->
-      let lexbuf = Lexing.from_channel ic in
-      Parser.deduction Lexer.read lexbuf)
+let deduction_of_ic ic =
+  let lexbuf = Lexing.from_channel ic in
+  Parser.deduction Lexer.read lexbuf
+
+let deduction_of_filename filename =
+  In_channel.with_file filename ~f:deduction_of_ic
 
 let string_of_result =
   function
   | Ok () -> "Deduction is correct"
-  | Error (i, m) -> Printf.sprintf "Error on line %d: %s\n" i m
+  | Error (i, m) -> Printf.sprintf "Error on line %d: %s" i m
 
 let () =
-  let filename = Caml.Sys.argv.(1) in
-  construct_deduction filename
-  |> Deduction.validate
+  let deduction =
+    match Caml.Sys.argv.(1) with
+    | s -> deduction_of_filename s
+    | exception Invalid_argument _ -> deduction_of_ic stdin
+  in
+  Deduction.validate deduction
   |> string_of_result
   |> printf "%s\n"
